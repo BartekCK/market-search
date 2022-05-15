938cf4eb3d1cadafd4bca555f3f24a3ff02fae53
Bartłomiej <bartlomiej.kotarski@netguru.com>
2022-05-15 23:25:07 +0200

    feat: :sparkles: validation pipe

--- market-search-be/src/main.ts
+++ market-search-be/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvConfigService } from './env-config/services/env-config.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get<EnvConfigService>(EnvConfigService);
   const { APP_PORT } = configService.getAppConfig();
+
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true, forbidNonWhitelisted: true }),
+  );
 
   await app.listen(APP_PORT);
 }
